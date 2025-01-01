a0bf413b50146cb58bbdb1ba69c55a54ab2ebd92
Kate Novak <kje@cochleoid.com>
2024-12-31 20:20:58 -0500

    Chapter 5, Exercise 7

--- src/ch05/ch05_exercise07.rs
+++ src/ch05/ch05_exercise07.rs
@@ -138,6 +138,55 @@ pub fn integrate_trapezoidal_rule_adaptive<F>(a: f64, b: f64, epsilon: f64, f: F
   approximation2
 }
 
+pub fn integrate_romberg<F>(a: f64, b: f64, epsilon: f64, f: F) -> f64
+  where F: Fn(f64) -> f64
+{
+  let mut n: usize = 1;
+  let mut h = (b - a) / (n as f64);
+
+  // Begin with an initial approximation.
+  let mut approximation1 = _integrate_trapezoidal_rule(a, b, n, h, &f);
+  let mut rs1: Vec<f64> = Vec::new();
+  let mut rs2: Vec<f64> = Vec::new();
+  rs1.push(approximation1);
+
+  println!("    Number of slices: {}, Estimated Error: --", n);
+
+  while n <= (1<<28) {
+    // Then calculate the next approximation by adding in samples which are between the
+    // previous approximation's samples.
+    n *= 2;
+    h /= 2.0;
+    let approximation2 = approximation1 / 2.0 + h * adaptive_sum_trapezoidal_rule(a, n, h, &f);
+    rs2.push(approximation2);
+    
+    print!("    ");
+    for k in 1..=rs1.len() {
+      let rkj = rs2[k-1] + (rs2[k-1] - rs1[k-1]) / ((4 as i64).pow(k as u32) - 1) as f64;
+      print!("{} ", rkj);
+      rs2.push(rkj);
+    }
+    println!();
+
+    // If the approximation is within our error bounds, we have our answer.
+    // See section 5.2.1, equation (5.28).
+    let error = (rs2[rs2.len() - 2] - rs1[rs2.len() - 2] ) / ((4 as i64).pow(rs1.len() as u32) - 1) as f64;
+
+    println!("    Number of slices: {}, Estimated Error: {}", n, error);
+
+    if error.abs() < epsilon {
+      break;
+    }
+
+    std::mem::swap(&mut rs1, &mut rs2);
+    rs2.clear();
+    approximation1 = approximation2;
+  }
+
+  rs1[rs1.len() - 1]
+}
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   // f(x) = ( sin(sqrt(100*x)) )^2
   // Integral from 0 to 1 is expected to be around 0.45.
@@ -181,11 +230,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!();
   integrate_trapezoidal_rule_adaptive(a, b, 1.0e-6, integrand);
   println!();
-  println!("   (bonus) with Simpson's Rule:");
+  println!("    (bonus) with Simpson's Rule:");
   println!();
   integrate_simpsons_rule_adaptive(a, b, 1.0e-6, integrand);
   println!();
 
+  println!("b) Integral of ( sin(sqrt(100*x)) )^2 from {} to {} with Romberg integration:", a, b);
+  println!();
+  integrate_romberg(a, b, 1.0e-6, integrand);
+  println!();
 
   Ok(())
 }
@@ -221,7 +274,7 @@ a) Integral of ( sin(sqrt(100*x)) )^2 from 0 to 1 with trapezoidal rule:
     Number of slices: 2048, Estimate of Integral: 0.45583063620164654, Estimated Error: 0.000001896023075524204
     Number of slices: 4096, Estimate of Integral: 0.455832058278271, Estimated Error: 0.0000004740255414859007
 
-   (bonus) with Simpson's Rule:
+    (bonus) with Simpson's Rule:
 
     Number of posts: 2, Estimate of Integral: 0.38431604889308213, Estimated Error: --
     Number of posts: 4, Estimate of Integral: 0.5746331650289503, Estimated Error: 0.012687807742391215
@@ -231,5 +284,21 @@ a) Integral of ( sin(sqrt(100*x)) )^2 from 0 to 1 with trapezoidal rule:
     Number of posts: 64, Estimate of Integral: 0.45574568635801116, Estimated Error: 0.00008181700486445954
     Number of posts: 128, Estimate of Integral: 0.45582702875861086, Estimated Error: 0.0000054228267066469545
     Number of posts: 256, Estimate of Integral: 0.45583218714672064, Estimated Error: 0.00000034389254065144335
+
+b) Integral of ( sin(sqrt(100*x)) )^2 from 0 to 1 with Romberg integration:
+
+    Number of slices: 1, Estimated Error: --
+    0.38431604889308213 
+    Number of slices: 2, Estimated Error: 0.05908414108660753
+    0.5746331650289505 0.5873209727713417 
+    Number of slices: 4, Estimated Error: 0.012687807742391222
+    0.3665689810632206 0.35269803546550527 0.34897386185747614 
+    Number of slices: 8, Estimated Error: -0.003724173608029149
+    0.4391386762335798 0.4439766559116038 0.4454255229028117 0.4458037647108326 
+    Number of slices: 16, Estimated Error: 0.0003782418080209237
+    0.4545184312850442 0.45554374828847516 0.4557273529293779 0.4557677522628154 0.4557774922310969 
+    Number of slices: 32, Estimated Error: 0.000009739968281508114
+    0.4557456863580111 0.4558275033628756 0.45583200741167557 0.45583241782141004 0.45583248103309976 0.45583249446137863 
+    Number of slices: 64, Estimated Error: 0.000000013428278877370225
 
 */
